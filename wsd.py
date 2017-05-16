98ef0ba6a6c44d889b881384e93872a63b2de2f0
adityarama1210 <adityarama1210@gmail.com>
2017-05-16 20:38:49 +0700

    update dictionary

--- wsd.py
+++ wsd.py
@@ -463,9 +463,9 @@ f_id_original = 'original_id_clean.txt'
 # sentence id original
 f_id_postag = 'original_id_clean_postag.txt'
 # sentence id pos tag
-f_dictionary = 'enhanced_dictionary_new.txt'
+#f_dictionary = 'enhanced_dictionary_new.txt'
 #f_dictionary = 'enhanced_dictionary.txt'
-#f_dictionary = 'enhanced_crawl.txt'
+f_dictionary = 'enhanced_dictionary_final.txt'
 # dictionary
 f_stopwords = 'stopwords.txt'
 # stopword file
